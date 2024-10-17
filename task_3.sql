bd83d90650301821c15dfa9d372fe58be2f4df30
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-10-17 10:06:50 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,3 @@
--- This script lists all the tables in the specified database
+-- This script lists all the tables in the alx_book_store database
 
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'alx_book_store';
+SHOW TABLES;
