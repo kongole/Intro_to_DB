88faf9b541e99c83f398dd9fbc89366ec5644c3c
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-10-17 09:49:46 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)  -- Foreign Key Constraint
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)  -- This is the correct foreign key reference
 );
 
 -- Create the Order_Details table
