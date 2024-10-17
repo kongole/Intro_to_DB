3241c159b87475d61b66a785b1901a691c14f52a
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-10-17 10:11:47 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 -- This script inserts a single row into the customers table in the alx_book_store database
 
-INSERT INTO Customers (customer_id, customer_name, email, address) 
+INSERT INTO customers (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
