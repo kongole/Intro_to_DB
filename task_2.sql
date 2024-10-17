84620002c3b6e0c8e27ded043da34241c3c84684
Festus Kiprotich <142789998+kongole@users.noreply.github.com>
2024-10-17 09:52:30 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create the Customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
